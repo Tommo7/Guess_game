830774a4e25a616e1f2649e0801be7f18c716207
Tommo7 <1301527@abertay.ac.uk>
2017-05-30 01:38:31 +0100

    Image loader class created for displaying the player image from JSON url, and included temporary image for testing.

--- app/src/main/java/com/example/tommo/guess_game/MainActivity.java
+++ app/src/main/java/com/example/tommo/guess_game/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.tommo.guess_game;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button begin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        begin = (Button)findViewById(R.id.Begin_button);
+        try {
+            if (begin != null) {
+                begin.setOnClickListener(this);
+            }
+        }
+        catch(NullPointerException e)
+        {
+            Log.e("Error", e.getMessage());
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == begin)
+            startActivity(new Intent(getApplicationContext(), DataListActivity.class));
     }
 }
